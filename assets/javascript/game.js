041947a31bb18dd007c8c41eacc64081aa5fd90d
Mobile <eddie.a.linares@gmail.com>
2019-04-04 21:45:51 -0500

    bluePylon fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ var numberGoal = Math.floor(Math.random() * 102) + 19;
 // var userOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var userOptions = Math.floor(Math.random() * 12) + 1;
 var userCount = 0;
+var win = 0;
+var lost = 0;
 var pylonBlue = $("#bluePylon");
 var pylonGreen = $("#greenPylon");
 var pylonYellow = $("#yellowPylon");
@@ -11,15 +13,7 @@ var pylonRed = $("#redPylon");
 // Tieing up the HTML div with the random number being created
 $("#randomNumber").text("Random number: " + numberGoal);
 
-// for (var i = 0; i < userOptions.length; i++) {
 
-//   pylonBlue.addClass("blue-image");
-
-//   pylonBlue.attr("data-pylonBlue", userOptions[i]);
-
-//   $("#bluePylon").append(pylonBlue);
-
-// }
 
 pylonBlue.attr("data-pylonBlue", userOptions);
 
@@ -34,19 +28,33 @@ $("#bluePylon").on("click", function () {
   $("#playerScore").text("Your current amount: " + userCount);
 
   if (userCount === numberGoal) {
-    $("#wins").text("Wins: " + 1);
-    numberGoal = Math.floor(Math.random() * 102) + 19;
-    userOptions = Math.floor(Math.random() * 12) + 1;
-    userCount = 0;
-    
+    countWinOrLoss('win')
   }
 
-  else if (userCount >= numberGoal) {
-    $("#lost").text("Lost: " + 1);
-    numberGoal = Math.floor(Math.random() * 102) + 19;
-    userOptions = Math.floor(Math.random() * 12) + 1;
-    userCount = 0;
-    
+  else if (userCount > numberGoal) {
+    countWinOrLoss('loss')
   }
 
 })
+// Function created for Win or Loss
+function countWinOrLoss(result) {
+  // If win do below
+  if (result === 'win') {
+    win++;
+    $("#wins").text("Wins: " + win);
+    // If loss do below
+  } else if (result === 'loss') {
+    lost++;
+    $("#lost").text("Lost: " + lost);
+  }
+  // This happens below to both win and loss since its not in the if and else if.
+  var newNumberGoal = Math.floor(Math.random() * 102) + 19;
+  $("#randomNumber").text("Random number: " + newNumberGoal);
+  userOptions = Math.floor(Math.random() * 12) + 1;
+  var newUserOption = Math.floor(Math.random() * 12) + 1;
+  pylonBlue.attr("data-pylonBlue", newUserOption);
+  $("#bluePylon").append(pylonBlue);
+
+  userCount = 0;
+  numberGoal = newNumberGoal;
+}
